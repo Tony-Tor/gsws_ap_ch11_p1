f8bf1c40089e5446e511955ac5fe9fd15bab11d7
Tony-Tor <Tony.Tor2016@yandex.ru>
2023-08-21 18:43:46 +0400

    Client

--- src/main/java/com/example/gsws_ap_ch11_p1/controllers/PaymentsController.java
+++ src/main/java/com/example/gsws_ap_ch11_p1/controllers/PaymentsController.java
@@ -1,12 +1,14 @@
 package com.example.gsws_ap_ch11_p1.controllers;
 
 import com.example.gsws_ap_ch11_p1.model.Payment;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
 import java.util.logging.Logger;
 
 @RestController
@@ -21,7 +23,11 @@ public class PaymentsController {
     ) {
         logger.info("Received request with ID " + requestId +
                 " ;Payment Amount: " + payment.getAmount());
-        payment.setId()
+        payment.setId(UUID.randomUUID().toString());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header("requestId")
+                .body(payment);
     }
 
 }
